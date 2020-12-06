91b180c39ae8aeade011f33997d53042515ae261
Alex Haynes <aheinous@gmail.com>
2020-12-06 15:48:50 -0800

    clean up and restructuring

--- compiler.py
+++ compiler.py
@@ -1,55 +1,18 @@
 #! /usr/bin/python3
 
-from os import name
 from lark import Lark
-from lark.visitors import Interpreter, Transformer, visit_children_decor
-
+from lark.visitors import Interpreter,  visit_children_decor
 from collections import namedtuple
 
 
-# json_parser = Lark(r"""
-#     ?value: dict
-#           | list
-#           | string
-#           | SIGNED_NUMBER      -> number
-#           | "true"             -> true
-#           | "false"            -> false
-#           | "null"             -> null
-
-#     list : "[" [value ("," value)*] "]"
-
-#     dict : "{" [pair ("," pair)*] "}"
-#     pair : string ":" value
-
-#     string : ESCAPED_STRING
-
-#     %import common.ESCAPED_STRING
-#     %import common.SIGNED_NUMBER
-#     %import common.WS
-#     %ignore WS
-
-#     """, start='value')
-
-# text = '{"key": ["item0", "item1", 3.14], "true":null, "false":false}'
-# tree = json_parser.parse(text)
-# print(tree.pretty())
 
 
-with  open('syntax.lark', 'r') as synfile:
-    syntax = ''.join(synfile.readlines())
 
-# print(syntax)
-
-parser = Lark(syntax)
+_Instr = namedtuple('Instr', ['op', 'data'])
 
 
-Sym = namedtuple('Sym', ['value'])
-
-_Cmd = namedtuple('Cmd', ['op', 'data'])
-
-
-def Cmd(op, *vargs):
-        return _Cmd(op, vargs)
+def Instr(op, *vargs):
+        return _Instr(op, vargs)
 
 
 class VMRuntimeError(Exception):
@@ -58,66 +21,91 @@ class VMRuntimeError(Exception):
 
 class VirtualMachine:
     def __init__(self):
-        self._cmds = []
+        self._instrs = []
         self._dataStack = []
         self._ctx = {}
 
-    def addCmd(self, cmd):
-        self._cmds.append(cmd)
 
-    def _pushData(self, data):
+    def _push(self, data):
         self._dataStack.append(data)
+
+    def _pop(self):
+        return self._dataStack.pop()
+
+    def addInstr(self, instr):
+        self._instrs.append(instr)
+
 
 
     def __str__(self):
-        return 'VM:\n\t' + str(self._cmds) + '\n\t' + str(self._dataStack) \
+        return 'VM:\n\t' + str(self._instrs) + '\n\t' + str(self._dataStack) \
                 + '\n\t' + str(self._ctx)
 
     def run(self):
-        for cmd in self._cmds:
-            if(cmd.op == 'add'):
-                right = self._dataStack.pop()
-                left = self._dataStack.pop()
-                self._dataStack.append(left + right)
-            elif(cmd.op == 'sub'):
-                right = self._dataStack.pop()
-                left = self._dataStack.pop()
-                self._dataStack.append(left - right)
-            elif(cmd.op == 'mult'):
-                right = self._dataStack.pop()
-                left = self._dataStack.pop()
-                self._dataStack.append(left * right)
-            elif(cmd.op == 'div'):
-                right = self._dataStack.pop()
-                left = self._dataStack.pop()
-                self._dataStack.append(left // right)
-            elif(cmd.op == 'neg'):
-                arg = self._dataStack.pop()
-                self._dataStack.append(arg * -1)
-
-            elif(cmd.op == 'assign'):
-                sym = cmd.data[0]
-                if sym not in self._ctx:
-                    raise VMRuntimeError('Attempt to assign to undeclared variable ' + sym)
-                self._ctx[sym] = self._dataStack.pop()
-            elif(cmd.op == 'decl'):
-                sym = cmd.data[0]
-                if sym in self._ctx:
-                    raise VMRuntimeError('Attempt to declare already declared vaiable ' + sym)
-                self._ctx[sym] = 0
+        for instr in self._instrs:
+            method = getattr(self, instr.op, None)
+            if method is None:
+                raise ValueError('Unrecognized instr.op: ' + instr.op)
+            method(instr.data)
 
 
 
-            elif(cmd.op == 'pushi'):
-                self._dataStack.append(cmd.data[0])
-            elif(cmd.op == 'push'):
-                self._dataStack.append(self._ctx[cmd.data[0]])
 
-            else:
-                raise ValueError('Unrecognized cmd.op: ' + cmd.op)
-        # assert len(self._dataStack) == 1
-        # return self._dataStack.pop()
+    def add(self, data):
+        right = self._pop()
+        left = self._pop()
+        self._push(left + right)
 
+
+    def sub(self, data):
+        right = self._pop()
+        left = self._pop()
+        self._push(left - right)
+
+
+    def mult(self, data):
+        right = self._pop()
+        left = self._pop()
+        self._push(left * right)
+
+
+    def div(self, data):
+        right = self._pop()
+        left = self._pop()
+        self._push(left // right)
+
+
+    def neg(self, data):
+        arg = self._pop()
+        self._push(arg * -1)
+
+
+    def assign(self, data):
+        sym = data[0]
+        if sym not in self._ctx:
+            raise VMRuntimeError('Attempt to assign to undeclared variable ' + sym)
+        self._ctx[sym] = self._pop()
+
+
+    def decl(self, data):
+        sym = data[0]
+        if sym in self._ctx:
+            raise VMRuntimeError('Attempt to declare already declared vaiable ' + sym)
+        self._ctx[sym] = 0
+
+
+    def pushi(self, data):
+        self._push(data[0])
+
+
+    def push(self, data):
+        self._push(self._ctx[data[0]])
+
+
+
+def getSymValue(sym):
+    assert sym.data == 'sym'
+    return str(sym.children[0])
 
 
 class TreeInterpreter(Interpreter):
@@ -127,62 +115,52 @@ class TreeInterpreter(Interpreter):
 
     @visit_children_decor
     def add(self, tree):
-        print('visit add')
-
+        self._vm.addInstr(Instr('add'))
 
     @visit_children_decor
     def sub(self, tree):
-        self._vm.addCmd(Cmd('sub'))
+        self._vm.addInstr(Instr('sub'))
 
     @visit_children_decor
     def mult(self, tree):
-        self._vm.addCmd(Cmd('mult'))
+        self._vm.addInstr(Instr('mult'))
 
     @visit_children_decor
     def div(self, tree):
-        self._vm.addCmd(Cmd('div'))
+        self._vm.addInstr(Instr('div'))
 
     @visit_children_decor
     def neg(self, tree):
-        self._vm.addCmd(Cmd('neg'))
+        self._vm.addInstr(Instr('neg'))
 
 
 
     def assign(self, tree):
         self.visit(tree.children[1])
-        sym = tree.children[0]
-        assert sym.data == 'sym'
-        # evaluate(children[1], vm)
-        self._vm.addCmd(Cmd('assign', str(sym)))
+        self._vm.addInstr(Instr('assign', getSymValue(tree.children[0])))
 
 
 
     def num(self, tree):
-        self._vm.addCmd(Cmd('pushi', int(tree.children[0]) ))
+        self._vm.addInstr(Instr('pushi', int(tree.children[0]) ))
 
 
     def sym(self, tree):
-        # print(tree)
-        # print(children)
-        self._vm.addCmd(Cmd('push', str( tree.children[0])))
-
+        self._vm.addInstr(Instr('push', getSymValue(tree)))
 
 
     def decl(self, tree):
         print('decl:', tree)
-        self._vm.addCmd(Cmd('decl', tree.children[0].value))
+        self._vm.addInstr(Instr('decl', getSymValue(tree.children[0])))
 
 
     def decl_init(self, tree):
-        # evaluate(tree.children[1], self._vm)
         self.visit(tree.children[1])
-        self._vm.addCmd(Cmd('decl', tree.children[0].value))
-        self._vm.addCmd(Cmd('assign',  tree.children[0].value))
+        self._vm.addInstr(Instr('decl',    getSymValue(tree.children[0])))
+        self._vm.addInstr(Instr('assign',  getSymValue(tree.children[0])))
 
 
-class PreInterpTransformer(Transformer):
-    def sym(self, tree):
-        return Sym(str(tree[0]))
+
 
 
 
@@ -195,19 +173,13 @@ def run(exprn):
     print(tree)
     print(tree.pretty())
 
-    print('### PreinterpTransformation')
-    preInterp = PreInterpTransformer()
-    tree = preInterp.transform((tree))
-    print(tree)
-    print(tree.pretty())
-
 
     print("### Tree interpreter")
     vm = VirtualMachine()
     ti = TreeInterpreter(vm)
 
     ti.visit(tree)
-    print('vm')
+    print(vm)
 
     print('### Run')
     try:
@@ -221,31 +193,39 @@ def run(exprn):
     print('---------------------------------------------------------------------')
 
 
+if __name__ == '__main__':
+    with  open('syntax.lark', 'r') as synfile:
+        syntax = ''.join(synfile.readlines())
+
+    parser = Lark(syntax)
 
 
+    # run('''
 
-run('''
+    # var x;
+    # '''
+    # )
 
-var x;
-'''
-)
+    # run('var x = 2;')
 
-run('var x = 2;')
+    # run('''
+    #  var x;
+    #  var x;
+    # ''')
 
-run('''
- var x;
- var x;
-''')
+    run('''
+        var x = 1;
+        var y = x - 20 + 10 * 3 / 4;
+        var z;
+        var w = 3*(x+1);
 
-run('''
+    ''')
 
-''')
+    # run('''
 
-run('''
+    # ''')
 
-''')
+    # run('''
 
-run('''
-
-''')
+    # ''')
 
