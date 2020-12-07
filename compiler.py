70e2bea8ee338e65fbf26be1168c5dd7cb15677c
Alex Haynes <aheinous@gmail.com>
2020-12-07 00:06:57 -0800

    if_elif while loop

--- compiler.py
+++ compiler.py
@@ -7,6 +7,7 @@ from collections import namedtuple
 def indent(strlist):
     for i in range(len(strlist)):
         strlist[i] = '\t' + strlist[i]
+    return strlist
 
 class Instr:
     def run(vm):
@@ -121,22 +122,93 @@ class Push(Instr):
     def compile(self, vm):
         return vm.comp_pushi(self._sym)
 
-class IfStatement(Instr):
-    def __init__(self, instrs):
-        self._instrs = instrs
+# class IfStatement(Instr):
+#     def __init__(self, instrs):
+#         self._instrs = instrs
+
+#     def run(self, vm):
+#         cond = vm.run_pop()
+#         if(cond):
+#             vm.run(self._instrs)
+
+#     def compile(self, vm):
+#         code = ['if(' + vm.comp_pop() + '){']
+#         block = vm.compile(self._instrs)
+#         indent(block)
+#         code += block
+#         code += ['}']
+#         return code
+
+
+class IfElse(Instr):
+    def __init__(self, condInstr, ifBlockInstr, elseBlockInstr):
+        self._condInstr = condInstr
+        self._ifBlockInstr = ifBlockInstr
+        self._elseBlockInstr = elseBlockInstr
+
 
     def run(self, vm):
+        vm.run(self._condInstr)
         cond = vm.run_pop()
-        if(cond):
-            vm.run(self._instrs)
+        if cond:
+            vm.run(self._ifBlockInstr)
+        else:
+            vm.run(self._elseBlockInstr)
 
     def compile(self, vm):
-        code = ['if(' + vm.comp_pop() + '){']
-        block = vm.compile(self._instrs)
-        indent(block)
-        code += block
+        code = vm.compile(self._condInstr)
+        code += ['if(' + vm.comp_pop() + '){']
+        code += indent(vm.compile(self._ifBlockInstr))
+        elseBlk = indent(vm.compile(self._elseBlockInstr))
+        if elseBlk:
+            code += ['} else {']
+            # code += indent(vm.compile(self._elseBlockInstr))
+            code += elseBlk
         code += ['}']
         return code
+
+
+
+
+
+class WhileLoop(Instr):
+    def __init__(self, condInstrs, loopInstrs):
+        self._condInstrs = condInstrs
+        self._loopInstrs = loopInstrs
+
+
+    def run(self, vm):
+        while True:
+            vm.run(self._condInstrs)
+            cond = vm.run_pop()
+            if not cond:
+                break
+            vm.run(self._loopInstrs)
+
+    def compile(self, vm):
+        code = []
+
+        # condCode= vm.compile(self._condInstrs)
+        # code += condCode
+        # code += ['while(' + vm.comp_pop() + '){']
+        # blockCode = vm.compile(self._loopInstrs)
+        # blockCode += condCode
+        # indent(blockCode)
+        # code += blockCode
+        # code += ['}']
+
+        code += ['while(1){']
+        blockCode = vm.compile(self._condInstrs)
+        blockCode += ['if(!(' + vm.comp_pop()+')){']
+        blockCode += indent(['break;'])
+        blockCode += ['}']
+        blockCode += vm.compile(self._loopInstrs)
+        code += indent(blockCode)
+        code += ['}']
+
+        return code
+
+
 
 
 
@@ -200,6 +272,8 @@ class VirtualMachine:
     def addInstr(self, instr):
         self._instrs().append(instr)
 
+    # def addIn
+
     def _instrs(self):
         return self._instrsDestStack[-1]
 
@@ -248,6 +322,11 @@ class TreeInterpreter(Interpreter):
         super().__init__()
         self._vm = vm
 
+    def visit_get_instrs(self, tree):
+        with self._vm.newInstrDest() as instrDest:
+            self.visit(tree)
+            return instrDest.getInstrs()
+
     @visit_children_decor
     def add(self, tree):
         self._vm.addInstr(Add())
@@ -294,14 +373,42 @@ class TreeInterpreter(Interpreter):
         self._vm.addInstr(Assign( getSymValue(tree.children[0])))
 
 
-    def if_statement(self, tree):
-        self.visit(tree.children[0])
+    # def if_statement(self, tree):
+    #     self.visit(tree.children[0])
+    #     with self._vm.newInstrDest() as instrDest:
+    #         self.visit(tree.children[1])
+    #         ifstatement = IfStatement(instrDest.getInstrs())
+    #     self._vm.addInstr(ifstatement)
+
+    def if_elif(self, tree):
+        children = tree.children
+        elseBlk = []
+        if len(children) % 2 == 1:
+            elseBlk = self.visit_get_instrs(children[-1])
+
+        start = len(children) - (len(children) % 2) - 2
+
+
+        for i in range(start, -2, -2):
+            cond =  self.visit_get_instrs(children[i])
+            ifBlk = self.visit_get_instrs(children[i+1])
+            elseBlk = [IfElse(cond, ifBlk, elseBlk)]
+
+        self._vm.addInstr(elseBlk[0])
+
+
+
+    def while_loop(self, tree):
+
+        with self._vm.newInstrDest() as instrDest:
+            self.visit(tree.children[0])
+            cond = instrDest.getInstrs()
+
         with self._vm.newInstrDest() as instrDest:
             self.visit(tree.children[1])
-            ifstatement = IfStatement(instrDest.getInstrs())
-        self._vm.addInstr(ifstatement)
-
-
+            loop = instrDest.getInstrs()
+        whileloop = WhileLoop(cond, loop)
+        self._vm.addInstr(whileloop)
 
 
 
@@ -385,3 +492,55 @@ if __name__ == '__main__':
         var y = 3 + z;
     ''')
 
+    run('''
+        var z = 1;
+        if(0 + z){
+            z = 2 + 0;
+            z = (2*z) / 2 + 1;
+            if(z){
+                z = z+1;
+            }
+        }
+        var y = 3 + z;
+    ''')
+
+
+    run('''
+        var accum = 1;
+        var num = 5;
+        while(num+2){
+            accum = accum*2;
+            num = num -1;
+        }
+    ''')
+
+
+    run('''
+        var x = 2;
+        var y;
+        if(x-2){
+            y =1;
+        }elif(x){
+            y = 2;
+        } elif(x-3) {
+            y = 3;
+            y = y + 1;
+        }else{
+            y = 400;
+        }
+
+    ''')
+
+    run('''
+        if(2){
+            var x = 3;
+        } else {
+
+        }
+    ''')
+
+    # run('''
+    # if(2){
+
+    # }
+    # ''')
