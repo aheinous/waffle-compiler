693575a7ca0fece6b0892e760b4e261223f96714
Alex Haynes <aheinous@gmail.com>
2020-12-06 20:48:29 -0800

    add if statement

--- compiler.py
+++ compiler.py
@@ -119,6 +119,37 @@ class Push(Instr):
     def compile(self, vm):
         return vm.comp_push(self._sym)
 
+class IfStatement(Instr):
+    def __init__(self, instrs):
+        self._instrs = instrs
+
+    def run(self, vm):
+        cond = vm.run_pop()
+        if(cond):
+            vm.run(self._instrs)
+
+    def compile(self, vm):
+        code = 'if( ' + vm.comp_pop() + '){\n'
+        code += vm.compile(self._instrs)
+        code += '\n}'
+        return code
+
+
+
+class RAIIInstrDest:
+    def __init__(self, vm, instrs):
+        self._vm = vm
+        self._instrs = instrs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        # print('__exit__: ', self, vargs)
+        self._vm._popInstrDest()
+
+    def getInstrs(self):
+        return self._instrs
 
 
 class VMRuntimeError(Exception):
@@ -127,7 +158,7 @@ class VMRuntimeError(Exception):
 
 class VirtualMachine:
     def __init__(self):
-        self._instrs = []
+        self._instrsDestStack = [[]]
 
         self._run_dataStack = []
         self.run_ctx = {}
@@ -153,22 +184,35 @@ class VirtualMachine:
         return self._run_dataStack.pop()
 
     def addInstr(self, instr):
-        self._instrs.append(instr)
+        self._instrs().append(instr)
 
+    def _instrs(self):
+        return self._instrsDestStack[-1]
+
+    def newInstrDest(self):
+        self._instrsDestStack.append([])
+        return RAIIInstrDest(self, self._instrsDestStack[-1])
+
+    def _popInstrDest(self):
+        self._instrsDestStack.pop()
 
 
     def __str__(self):
-        return 'VM:\n\t' + str(self._instrs) + '\n\t' + str(self._run_dataStack) \
+        return 'VM:\n\t' + str(self._instrs()) + '\n\t' + str(self._run_dataStack) \
                 + '\n\t' + str(self.run_ctx)
 
 
-    def run(self):
-        for instr in self._instrs:
+    def run(self, instrs=None):
+        if instrs is None:
+            instrs = self._instrs()
+        for instr in instrs:
             instr.run(self)
 
-    def compile(self):
+    def compile(self, instrs=None):
+        if instrs is None:
+            instrs = self._instrs()
         code = []
-        for instr in self._instrs:
+        for instr in instrs:
             code.append(instr.compile(self))
         return '\n'.join(code)
 
@@ -229,6 +273,16 @@ class TreeInterpreter(Interpreter):
         self.visit(tree.children[1])
         self._vm.addInstr(Decl( getSymValue(tree.children[0])))
         self._vm.addInstr(Assign( getSymValue(tree.children[0])))
+
+
+    def if_statement(self, tree):
+        self.visit(tree.children[0])
+        with self._vm.newInstrDest() as instrDest:
+            self.visit(tree.children[1])
+            ifstatement = IfStatement(instrDest.getInstrs())
+        self._vm.addInstr(ifstatement)
+
+
 
 
 
@@ -303,7 +357,10 @@ if __name__ == '__main__':
         var z = y - x;
     ''')
 
-    # run('''
-
-    # ''')
+    run('''
+        if(0){
+            var x = 2;
+        }
+        var y = 3;
+    ''')
 
