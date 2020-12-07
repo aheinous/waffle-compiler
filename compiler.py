744f15bdd452de3c6071ac294f67b382a960bed6
Alex Haynes <aheinous@gmail.com>
2020-12-06 16:27:42 -0800

    add basic compilation

--- compiler.py
+++ compiler.py
@@ -25,6 +25,20 @@ class VirtualMachine:
         self._dataStack = []
         self._ctx = {}
 
+        self.c_varStack = []
+        self.c_ctx = {}
+        self.c_code = ''
+        self.c_varCnt = 0
+
+    def c_pushVar(self, val):
+        name = 'tmp_{}'.format(self.c_varCnt)
+        self.c_varStack.append(name)
+        self.c_code += 'int {} = {};\n'.format(name, val)
+        self.c_varCnt += 1
+
+    def c_pop(self):
+        return self.c_varStack.pop()
+
 
     def _push(self, data):
         self._dataStack.append(data)
@@ -48,6 +62,15 @@ class VirtualMachine:
                 raise ValueError('Unrecognized instr.op: ' + instr.op)
             method(instr.data)
 
+    def compile(self):
+        for instr in self._instrs:
+            method = getattr(self, 'c_' + instr.op, None)
+            if method is None:
+                raise ValueError('Unrecognized instr.op: ' + instr.op)
+            method(instr.data)
+        return self.c_code
+
+
 
 
 
@@ -56,17 +79,33 @@ class VirtualMachine:
         left = self._pop()
         self._push(left + right)
 
+    def c_add(self, data):
+        right = self.c_pop()
+        left = self.c_pop()
+        self.c_pushVar('{} + {}'.format(left, right))
 
     def sub(self, data):
         right = self._pop()
         left = self._pop()
         self._push(left - right)
 
+    def c_sub(self, data):
+        right = self.c_pop()
+        left = self.c_pop()
+        self.c_pushVar('{} - {}'.format(left, right))
+
+
+
 
     def mult(self, data):
         right = self._pop()
         left = self._pop()
         self._push(left * right)
+
+    def c_mult(self, data):
+        right = self.c_pop()
+        left = self.c_pop()
+        self.c_pushVar('{} * {}'.format(left, right))
 
 
     def div(self, data):
@@ -75,9 +114,19 @@ class VirtualMachine:
         self._push(left // right)
 
 
+    def c_div(self, data):
+        right = self.c_pop()
+        left = self.c_pop()
+        self.c_pushVar('{} / {}'.format(left, right))
+
+
     def neg(self, data):
         arg = self._pop()
         self._push(arg * -1)
+
+    def c_neg(self, data):
+        arg = self.c_pop()
+        self.c_pushVar('-{}'.format(arg))
 
 
     def assign(self, data):
@@ -87,19 +136,43 @@ class VirtualMachine:
         self._ctx[sym] = self._pop()
 
 
+    def c_assign(self, data):
+        sym = data[0]
+        # if sym not in self._ctx:
+        #     raise VMRuntimeError('Attempt to assign to undeclared variable ' + sym)
+        # self._ctx[sym] = self._pop()
+
+        self.c_code += '{} = {};\n'.format(sym, self.c_pop() )
+
+
+
+
     def decl(self, data):
         sym = data[0]
         if sym in self._ctx:
             raise VMRuntimeError('Attempt to declare already declared vaiable ' + sym)
         self._ctx[sym] = 0
 
+    def c_decl(self, data):
+        sym = data[0]
+        self.c_code += 'int {};\n'.format(sym)
+
+
 
     def pushi(self, data):
         self._push(data[0])
 
 
+    def c_pushi(self, data):
+        self.c_pushVar(data[0])
+
+
     def push(self, data):
         self._push(self._ctx[data[0]])
+
+    def c_push(self, data):
+        self.c_push(data[0])
+
 
 
 
@@ -190,6 +263,11 @@ def run(exprn):
     print(vm)
 
 
+    print('### compile')
+    code = vm.compile()
+    print(code)
+
+
     print('---------------------------------------------------------------------')
 
 
@@ -214,10 +292,7 @@ if __name__ == '__main__':
     # ''')
 
     run('''
-        var x = 1;
-        var y = x - 20 + 10 * 3 / 4;
-        var z;
-        var w = 3*(x+1);
+        var x = 1 * (3+4) / 1000 -10;
 
     ''')
 
